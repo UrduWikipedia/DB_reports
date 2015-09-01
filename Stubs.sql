c06bc5e1ce657d74ea2fd315d0cc5fb60fa49d75
shuaib <mshuaibnadwi@gmail.com>
2015-09-02 01:17:13 +0530

    Update Stubs.sql

--- Stubs.sql
+++ Stubs.sql
@@ -1,1 +1,11 @@
-select concat('[[',p.page_title,']]') as Article_Name,concat('[[User:',r.rev_user_text,'|',r.rev_user_text,']]') as User_Name,p.page_len as Size from revision r, page p where r.rev_parent_id = 0 and r.rev_len < 2048 and p.page_len < 2048 and p.page_namespace = 0 and p.page_is_redirect = 0 and rev_timestamp < '2015900000000' and rev_timestamp > '20150731235959' and r.rev_page = p.page_id;
+SELECT CONCAT('[[',p.page_title,']]') as نام_مضمون,
+CONCAT('[[User:',r.rev_user_text,'|',r.rev_user_text,']]') as صارف_نام,
+p.page_len as Size from revision r, page p 
+WHERE r.rev_parent_id = 0 
+AND r.rev_len < 2048 
+AND p.page_len < 2048 
+AND p.page_namespace = 0 
+AND p.page_is_redirect = 0 
+AND rev_timestamp < '2015900000000' 
+AND rev_timestamp > '20150731235959' 
+AND r.rev_page = p.page_id;
